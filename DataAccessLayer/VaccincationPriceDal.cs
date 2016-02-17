3d8ae5f1abe4a28f369299278aa4ff5933d026eb
Phuluso Justin Musetsho <mbulungom41@gmail.com>
2016-02-17 09:54:39 +0200

    TN--03 : Dal Layer and EDMX for data Access

---
+++ DataAccessLayer/VaccincationPriceDal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityModelContainer;
+
+
+
+namespace DataAccessLayer
+{
+    public class VaccincationPriceDal
+    {
+        public bool AddVaccinationPrice(string inputVaccinationDefId, decimal inputAmount)
+        {
+            try
+            {
+                
+                using (var db = new VaccinationManagerEntities())
+                {
+                    var obj = new VaccincationPrice
+                    {
+                        VaccinationDefId = inputVaccinationDefId,
+                        PriceAmount = inputAmount
+                    };
+
+                    db.VaccincationPrices.Add(obj);
+                    db.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public decimal FindPriceByFaccinationId(string inputVaccinationDefId)
+        {
+            try
+            {
+                using (var db = new VaccinationManagerEntities())
+                {
+                    var obj = db.VaccincationPrices.FirstOrDefault(p => p.VaccinationDefId == inputVaccinationDefId);
+                    if (obj == null)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return obj.PriceAmount;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                return -1;
+            }
+        }
+    }
+}
