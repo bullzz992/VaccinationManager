b055e56fd7bf452bcd15c4f6955b8fd0e178ae6f
Phuluso Justin Musetsho <mbulungom41@gmail.com>
2016-03-11 08:16:08 +0200

    TN--21 - Done

---
+++ Models/Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaccinationManager.Models
+{
+    public class Invoice
+    {
+        public Child PatientChild;
+        public List<Vaccination> VaccinationList;
+        public List<ExtendedFee> ExtenddFeeList;
+
+    }
+}
