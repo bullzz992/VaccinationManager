5c0e62226b32dd56516192095d79eb4e9b37a5fb
Phuluso Justin Musetsho <mbulungom41@gmail.com>
2016-03-11 13:13:01 +0200

    Scripts for DB Sync

--- SQL/VaccinationPriceCREATE_1.sql
+++ SQL/VaccinationPriceCREATE_1.sql
@@ -1,27 +1,23 @@
-﻿--USE [VaccinationManager]
---GO
+﻿USE [VaccinationManager]
+GO
 
---/****** Object:  Table [dbo].[VaccincationPrice]    Script Date: 2/12/2016 8:55:25 AM ******/
---SET ANSI_NULLS ON
---GO
+/****** Object:  Table [dbo].[VaccinationPrices]    Script Date: 3/11/2016 12:19:44 PM ******/
+SET ANSI_NULLS ON
+GO
 
---SET QUOTED_IDENTIFIER ON
---GO
+SET QUOTED_IDENTIFIER ON
+GO
 
---SET ANSI_PADDING ON
---GO
+CREATE TABLE [dbo].[VaccinationPrices](
+	[VaccinationPriceId] [int] IDENTITY(1,1) NOT NULL,
+	[VaccinationDefId] [nvarchar](max) NULL,
+	[PriceAmount] [decimal](18, 2) NOT NULL,
+ CONSTRAINT [PK_dbo.VaccinationPrices] PRIMARY KEY CLUSTERED 
+(
+	[VaccinationPriceId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
---CREATE TABLE [dbo].[VaccincationPrice](
---	[VaccinationPriceId] [int] IDENTITY(1,1) NOT NULL,
---	[VaccinationDefId] [varchar](max) NOT NULL,
---	[PriceAmount] [money] NOT NULL,
--- CONSTRAINT [PK_VaccincationPrice] PRIMARY KEY CLUSTERED 
---(
---	[VaccinationPriceId] ASC
---)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
---) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
 
---GO
 
---SET ANSI_PADDING OFF
---GO
